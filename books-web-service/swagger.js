71baae5bf5138ec2f315a1405c9f807b28965eef
Martha <marthasibiya25@gmail.com>
2026-09-24 00:03:22 +0200

    Fix Swagger Render server URL

--- books-web-service/swagger.js
+++ books-web-service/swagger.js
@@ -7,7 +7,7 @@ const swaggerDocument = {
     },
     servers: [
         {
-            url: "http://localhost:3000",
+            url: "https://books-web-service.onrender.com",
         },
     ],
     paths: {
